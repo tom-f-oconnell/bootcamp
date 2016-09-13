35dfd86b10018af8f8e29edecce8a8f39f60ce03
Tom O'Connell <toconnel@caltech.edu>
2016-09-13 14:55:02 -0700

    Added doctstring for ecdf.

--- created/2/bootcamp_utils.py
+++ created/2/bootcamp_utils.py
@@ -2,6 +2,8 @@
 import numpy as np
 
 def ecdf(x):
+    """ Computes empirical cumulative probability distribution for data x. """
+    
     x_sorted = np.sort(x)
     # give each element equal incremental probability
     y = np.linspace(0,1,len(x))
