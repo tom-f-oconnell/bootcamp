f968d5efe17540fc903e26391aefd7a074fb4f25
Tom O'Connell <toconnel@caltech.edu>
2016-09-15 13:41:29 -0700

    Import logical fix.

--- created/4/morpho.py
+++ created/4/morpho.py
@@ -8,8 +8,8 @@ import scipy.optimize as sopt
 def gradient_model(x, I_0, a, lam):
     """ Model for Bcd gradient: exponential decay plus a constant background. """
 
-    assert np.all(np.array(x) > 0)
-    assert np.all(np.array([I_0, a, lam]) >= 0)
+    assert np.any(np.array(x) >= 0)
+    assert np.any(np.array([I_0, a, lam]) >= 0)
 
     return a + I_0 * np.exp(-x / lam)
 
@@ -19,3 +19,15 @@ df = df.rename(columns={'fractional distance from anterior': 'x', '[bcd] (a.u.)'
 plt.plot(df['x'], df['I_bcd'], marker='.', linestyle='none')
 plt.show()
 
+a_guess = 0.2
+I_0_guess = 0.9 - 0.2
+lam_guess = 0.25
+p0 = np.array([I_0_guess, a_guess, lam_guess])
+
+popt, _ = sopt.curve_fit(gradient_model, df['x'], df['I_bcd'], p0=p0)
+
+x_smooth = np.linspace(0, 1, 200)
+
+I_smooth = gradient_model(x_smooth, *tuple(popt))
+
+plt.plot(x_smooth, I_smooth, color='gray')
