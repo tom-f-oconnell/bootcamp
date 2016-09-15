60bc9c7fa15f44ced525af8b8ba5bd0adbbacefc
Tom O'Connell <toconnel@caltech.edu>
2016-09-14 20:13:59 -0700

    Finally got the pandas data in the right format.

--- created/3/grant_finches.py
+++ created/3/grant_finches.py
@@ -9,6 +9,24 @@ finch_1975 = pd.read_csv('../../data/grant_1975.csv', comment='#')
 finch_1987 = pd.read_csv('../../data/grant_1987.csv', comment='#')
 finch_1991 = pd.read_csv('../../data/grant_1991.csv', comment='#')
 finch_2012 = pd.read_csv('../../data/grant_2012.csv', comment='#')
-#?
+
+#finch_1973['year'] = np.ones(len(finch_1973)) * 1973
+finch_1975['year'] = np.ones(len(finch_1975)) * 1975
+finch_1987['year'] = np.ones(len(finch_1987)) * 1987
+finch_1991['year'] = np.ones(len(finch_1991)) * 1991
+finch_2012['year'] = np.ones(len(finch_2012)) * 2012
+
+std_name = {'Beak length, mm': 'beak length (mm)', 'Beak depth, mm': 'beak depth (mm)', \
+            'yearband': 'year', 'blength': 'beak length (mm)', 'beak depth': 'beak depth (mm)'\
+            ,'beak length': 'beak length (mm)', 'bdepth': 'beak depth (mm)'}
+finch_1973 = finch_1973.rename(columns=std_name)
+finch_1975 = finch_1975.rename(columns=std_name)
+finch_1987 = finch_1987.rename(columns=std_name)
+finch_1991 = finch_1991.rename(columns=std_name)
+finch_2012 = finch_2012.rename(columns=std_name)
+
+df = pd.concat((finch_1973, finch_1975, finch_1987, finch_1991, finch_2012), axis=0)
+
+# can compare to finch_complete
 finch_complete = pd.read_csv('../../data/grant_complete.csv', comment='#')
 
