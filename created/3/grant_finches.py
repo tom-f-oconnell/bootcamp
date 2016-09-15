a9eb032329f88574c9ce72936c0b8649d44f92f8
Tom O'Connell <toconnel@caltech.edu>
2016-09-14 20:45:04 -0700

    Plotting evolutionary divergences

--- created/3/grant_finches.py
+++ created/3/grant_finches.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import stats
+
 finch_1973 = pd.read_csv('../../data/grant_1973.csv', comment='#')
 finch_1975 = pd.read_csv('../../data/grant_1975.csv', comment='#')
 finch_1987 = pd.read_csv('../../data/grant_1987.csv', comment='#')
@@ -36,5 +38,48 @@ df.drop_duplicates(subset={'year','band'}, inplace=True)
 # save the file. can maybe diff to compare if same file is gauranteed with these params
 df.to_csv('../../my_grant_complete.csv', index=False)
 
+""" 4.1 data analysis """
 
+fortis = df.loc[(df['species'] == 'fortis') & (df['year'] == 1987)]
+scandens = df.loc[(df['species'] == 'scandens') & (df['year'] == 1987)]
 
+"""
+print(fortis['beak depth (mm)'])
+print(fortis['beak length (mm)'])
+print(scandens['beak depth (mm)'])
+print(scandens['beak length (mm)'])
+"""
+
+fortis_depth, fortis_d_cpx = stats.ecdf(fortis['beak depth (mm)'])
+scandens_depth, scandens_d_cpx = stats.ecdf(scandens['beak depth (mm)'])
+
+fortis_length, fortis_d_cpx = stats.ecdf(fortis['beak length (mm)'])
+scandens_length, scandens_d_cpx = stats.ecdf(scandens['beak length (mm)'])
+
+plt.plot(fortis_depth, fortis_d_cpx, marker='.', linestyle='none')
+plt.plot(scandens_depth, scandens_d_cpx, marker='.', linestyle='none')
+plt.title('Distribution of beak depths in 1987')
+plt.ylabel('ECDF')
+plt.xlabel('Beak depth (mm)')
+plt.legend(('Fortis', 'Scandens'))
+plt.margins(0.02)
+
+plt.figure()
+plt.plot(fortis_length, fortis_d_cpx, marker='.', linestyle='none')
+plt.plot(scandens_length, scandens_d_cpx, marker='.', linestyle='none')
+plt.title('Distribution of beak lengths in 1987')
+plt.ylabel('ECDF')
+plt.xlabel('Beak length (mm)')
+plt.legend(('Fortis', 'Scandens'))
+plt.margins(0.02)
+
+plt.figure()
+plt.plot(fortis_length, fortis_depth, marker='.', linestyle='none', color='b')
+plt.plot(scandens_length, scandens_depth, marker='.', linestyle='none', color='r')
+plt.title('Distribution of beak lengths in 1987')
+plt.ylabel('Beak depth (mm)')
+plt.xlabel('Beak length (mm)')
+plt.legend(('Fortis', 'Scandens'))
+plt.margins(0.02)
+
+plt.show()
